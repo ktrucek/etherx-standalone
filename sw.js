28deb6712b46cfd35779da5d3251e44104891149
ktrucek <ktrucek@gmail.com>
2026-05-20 00:35:16 +0200

    v2.4.188: Version bump

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Provides: Offline support, caching, background sync
 // ============================================================
 
-const CACHE_VERSION = 'etherx-v2.4.187';
+const CACHE_VERSION = 'etherx-v2.4.188';
 const CACHE_ASSETS = [
     '/etherx-standalone/src/index.html',
     '/etherx-standalone/assets/filters/filters.txt',
