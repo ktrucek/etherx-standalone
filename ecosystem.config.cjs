e4784663beb3d27e894b3a07ac93b3bcbd5614ee
ktrucek <ktrucek@gmail.com>
2026-06-24 23:44:29 +0200

    v3.0.120: Version bump

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
     apps: [
         {
             name: "etherx-browser",
-            script: "./node_modules/.bin/electron",
+            script: path.resolve(__dirname, 'node_modules', '.bin', 'electron'),
             args: [".", "--no-sandbox"],
             interpreter: "none",
             cwd: __dirname,
